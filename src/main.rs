46963d7b6497abf6e0792b4784f60714534ce91c
Maxim Ivanitskiy <ivanitskiy@gmail.com>
2021-12-17 13:43:41 -0800

    Open journal

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+use std::process;
+
+use systemd::{Journal, journal};
+
+
+
 fn main() {
     println!("Hello, world!");
+
+    // let mut journal = Journal::open(JournalFiles::All, false, true)
+    //     .expect("Failed to open systemd journal");
+
+    let mut j = journal::OpenOptions::default().local_only(true).current_user(true).open().unwrap();
+
+    match j.seek_head() {
+        Ok(cursor) => println!("opened {}", cursor),
+        Err(e) => {
+            print!("failed {}", e);
+            process::exit(1)
+        }
+    }
 }
